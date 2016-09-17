c9736f9255c490e174cd7722632d1a40f9bbd7f6
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2016-09-17 20:06:24 +1000

    Add initail project

---
+++ src/WebApplication/Models/CreateEventViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class CreateEventViewModel : EventModel
+    {
+        [Display(Name="Number of tickets")]
+        public Int32 NumberOfTickets { get; set; }
+    }
+}
---
+++ src/WebApplication/Models/EventModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class EventModel
+    {
+        /// <summary>
+        /// Public key
+        /// </summary>
+        public String Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
